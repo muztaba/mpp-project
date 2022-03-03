f4e1ef37328d7ce0645aae5af553e9402d0563fe
muztaba <hasanat.muztaba@gmail.com>
2022-03-03 14:22:23 -0600

    Mark as Serializable

--- src/main/java/com/mpp/model/Role.java
+++ src/main/java/com/mpp/model/Role.java
@@ -1,5 +1,7 @@
 package com.mpp.model;
 
-public enum Role {
+import java.io.Serializable;
+
+public enum Role implements Serializable {
     LIBRARIAN, ADMIN
 }
--- src/main/java/com/mpp/model/User.java
+++ src/main/java/com/mpp/model/User.java
@@ -1,8 +1,9 @@
 package com.mpp.model;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class User {
+public class User implements Serializable {
 
     private final String username;
 
