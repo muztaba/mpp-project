d5f83af326f6335c530b67b15e52a4ef6c4e0b2f
rayhanrashid <12345678asdf>
2022-03-04 02:04:42 -0600

    add-book :  added authorization for add book

---
+++ src/main/java/com/mpp/ui/AddLibraryMember.java
@@ -0,0 +1,53 @@
+package com.mpp.ui;
+
+import com.mpp.controller.BookController;
+import com.mpp.controller.ControllerFactory;
+import com.mpp.controller.LibraryMemberController;
+import com.mpp.model.Book;
+import com.mpp.model.LibraryMember;
+import com.mpp.model.Role;
+import com.mpp.utils.ApplicationContext;
+import com.mpp.utils.UIContext;
+import com.mpp.utils.Validator;
+import com.mpp.utils.WrongInput;
+
+import java.nio.file.AccessDeniedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddLibraryMember {
+
+
+    public static void showUI() {
+        try {
+            ApplicationContext.getAuthenticationConroller().hasPermission(ApplicationContext.getUser(), Role.ADMIN);
+        } catch (AccessDeniedException e) {
+            e.printStackTrace();
+            System.out.println("Access Denied! You have no Power Here!");
+            UserMenu.showUI(UIContext.getInstance());
+        }
+
+        LibraryMemberController libraryMemberController = ControllerFactory.getLibraryMemberController();
+        System.out.println("Please enter first Name");
+        Scanner sc = UIContext.getInstance().getSc();
+        String fname = sc.nextLine();
+        System.out.println("Please enter Last Name");
+        String lname = sc.nextLine();
+        System.out.println("Please enter phone Number ");
+        int phoneNumber = Integer.parseInt(sc.nextLine());
+        System.out.println("Please enter street");
+        String street = sc.nextLine();
+        System.out.println("Please enter city");
+        String city = sc.nextLine();
+        System.out.println("Please enter state");
+        String state = sc.nextLine();
+        System.out.println("Please enter zip");
+        int zip = Integer.parseInt(sc.nextLine());
+
+        LibraryMember libraryMember = libraryMemberController.createLibraryMember(fname, lname, phoneNumber,
+                street, city, state, zip);
+        System.out.println("Added New Library Member");
+        System.out.println(libraryMember);
+    }
+}
