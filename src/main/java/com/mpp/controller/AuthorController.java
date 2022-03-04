7d0bf0c934616096f7d7a02f16b9a62e0af6d1c4
Md Shajedul Islam <j.shajid@gmail.com>
2022-03-04 02:48:50 -0600

    refactor-getAuthorByName : refactored.

--- src/main/java/com/mpp/controller/AuthorController.java
+++ src/main/java/com/mpp/controller/AuthorController.java
@@ -3,12 +3,12 @@ package com.mpp.controller;
 import com.mpp.deserializer.DeserializerFactory;
 import com.mpp.deserializer.IDeserializer;
 import com.mpp.model.Author;
+import com.mpp.repository.AuthorRepository;
+import com.mpp.repository.RepositoryFactory;
 
 public class AuthorController implements DomainController {
     public Author getAuthorByName(String authorName) {
-        // TODO: get author by name from AuthorDeserializer
-        IDeserializer authorDeserializer = DeserializerFactory.getDeserializer(Author.class);
-        Author author = (Author) authorDeserializer.deserialize(authorName);
+        Author author = ((AuthorRepository) RepositoryFactory.getRepository(Author.class)).findByName(authorName);
         return author;
 
     }
