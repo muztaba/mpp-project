652ecff3b0edef62006d820c100c3d23307049c5
muztaba <hasanat.muztaba@gmail.com>
2022-03-04 23:01:20 -0600

    add more features

--- src/main/java/com/mpp/model/CheckoutRecord.java
+++ src/main/java/com/mpp/model/CheckoutRecord.java
@@ -5,19 +5,13 @@ import java.util.List;
 
 public class CheckoutRecord implements Identifiable {
     private List<CheckoutEntry> checkoutEntries;
-    private List<LateReturn> lateReturns;
 
     public CheckoutRecord() {
         checkoutEntries = new ArrayList<CheckoutEntry>();
-        lateReturns = new ArrayList<LateReturn>();
     }
 
     public void addCheckoutEntry(CheckoutEntry checkoutEntry) {
         checkoutEntries.add(checkoutEntry);
-    }
-
-    public void addLateReturn(LateReturn lateReturn) {
-        lateReturns.add(lateReturn);
     }
 
     public List<CheckoutEntry> getCheckoutEntries() {
@@ -26,14 +20,6 @@ public class CheckoutRecord implements Identifiable {
 
     public void setCheckoutEntries(List<CheckoutEntry> checkoutEntries) {
         this.checkoutEntries = checkoutEntries;
-    }
-
-    public List<LateReturn> getLateReturns() {
-        return lateReturns;
-    }
-
-    public void setLateReturns(List<LateReturn> lateReturns) {
-        this.lateReturns = lateReturns;
     }
 
     @Override
