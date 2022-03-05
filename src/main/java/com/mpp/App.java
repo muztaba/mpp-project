5c11ee4df584da40be6d3e70ad693c782e35fada
rayhanrashid <12345678asdf>
2022-03-05 05:52:38 -0600

    added bootloadin

--- src/main/java/com/mpp/App.java
+++ src/main/java/com/mpp/App.java
@@ -1,7 +1,10 @@
 package com.mpp;
 
-import com.mpp.model.Role;
-import com.mpp.model.User;
+import com.mpp.controller.AuthorController;
+import com.mpp.controller.BookController;
+import com.mpp.controller.ControllerFactory;
+import com.mpp.controller.LibraryMemberController;
+import com.mpp.model.*;
 import com.mpp.serializer.Storage;
 import com.mpp.serializer.StorageSerializer;
 import com.mpp.ui.LoginPage;
@@ -23,8 +26,28 @@ public class App {
             "124",
             new User("library", "library", List.of(Role.LIBRARIAN)),
             "125",
-            new User("admin1","admin", List.of(Role.ADMIN))
+            new User("admin1", "admin", List.of(Role.ADMIN))
     );
+
+
+    public static void bootLoader() {
+        AuthorController authorController = (AuthorController) ControllerFactory.getController(Author.class);
+        BookController bookController = (BookController) ControllerFactory.getController(Book.class);
+        LibraryMemberController libraryMemberController = (LibraryMemberController) ControllerFactory.getController(LibraryMember.class);
+        try {
+            authorController.createAuthor("dan", "Brown", 1234567890, "rasta",
+                    "shohor", "rastro", 52557, "Good Writer");
+            authorController.createAuthor("paul", "Brown", 1234567890, "1000 n",
+                    "fairfeild", "iowa", 52556, "Ok Writer");
+            bookController.addNewBook("Inferno", 1234, 7, List.of("dan"));
+            bookController.addNewBook("Not Inferno", 1235, 21, List.of("paul"));
+            libraryMemberController.createLibraryMember("Uzbuk", "Jolil", 0987654, "street", "city", "state", 555555);
+            libraryMemberController.createLibraryMember("Harami", "lname", 54321678, "street", "city", "state", 22222);
+        } catch (Exception e) {
+            System.out.println("Matha Karap!");
+        }
+
+    }
 
     public static void main(String[] args) {
         // Application Entry Point
@@ -32,9 +55,10 @@ public class App {
     }
 
     public void run() {
+        bootLoader();
         Scanner sc = new Scanner(System.in);
         CommandParser cp = new CommandParser();
-        UIContext uiContext = UIContext.getInstance(sc,cp);
+        UIContext uiContext = UIContext.getInstance(sc, cp);
         var serializer = new StorageSerializer(storageLocation);
         ApplicationContext.storage = (Storage) serializer.deserialize();
 
